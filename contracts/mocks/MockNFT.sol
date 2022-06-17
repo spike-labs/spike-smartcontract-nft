22e5c1d731dc7cec333e643156df4bc7542290c3
Fueav <104292973+Fueav@users.noreply.github.com>
2022-06-17 10:34:47 +0800

    Update MockNFT.sol

--- contracts/mocks/MockNFT.sol
+++ contracts/mocks/MockNFT.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
-import "../MetaverseNFT.sol";
 
 contract MockNFT is ERC721PresetMinterPauserAutoId {
     constructor(string memory name, string memory symbol) ERC721PresetMinterPauserAutoId(name, symbol, "") {
